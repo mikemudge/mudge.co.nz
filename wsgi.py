53ad8df772e3b303f163c03ac24b920fc4953a9f
Mike Mudge <mike.mudge@gmail.com>
2017-06-03 17:00:54 +1200

    update to not use config.

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,13 @@
 from app import create_app
 
-application = create_app()
+import os
+
+if os.environ.get('APP_SETTINGS'):
+    config = os.environ.get('APP_SETTINGS')
+else:
+    config = 'settings.production'
+
+application = create_app(config)
 
 if __name__ == "__main__":
     application.run()

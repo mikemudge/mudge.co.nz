1668d0564193af6a1d27235ac1b01edc858b835c
Mike Mudge <mike.mudge@gmail.com>
2017-09-09 16:22:56 +1200

    Update API and STATIC urls for prod

--- settings/production.py
+++ settings/production.py
@@ -4,6 +4,9 @@ ENV = 'production'
 
 SENTRY_DSN = 'https://9a549cab984c4f91a005e31bf7aeb0ed:5a33f71d3fd9434586c1938ab4e7572b@sentry.io/175555'
 
+API_URL = 'https://mudge.co.nz/'
+STATIC_URL = 'https://mudge.co.nz/'
+
 try:
     # import the local config to override for local settings
     from local_config import *

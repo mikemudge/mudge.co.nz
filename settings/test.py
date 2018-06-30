7d97cdf9ee9f619561885b047b63bbe65f01b88c
Mike Mudge <mike.mudge@gmail.com>
2018-07-01 09:15:43 +1200

    testing push

--- settings/test.py
+++ settings/test.py
@@ -7,6 +7,8 @@ SQLALCHEMY_DATABASE_URI = 'postgres://mudgeconzTest:test_password@db/mudgeconzTe
 PRESERVE_CONTEXT_ON_EXCEPTION = False
 SECRET_KEY = "Testing Secret"
 
+# TODO No local_config for testing.
+# Because all settings should be included here. where tests are run should be irrelevent?
 try:
     # import the local config to override for local settings
     from local_config import *

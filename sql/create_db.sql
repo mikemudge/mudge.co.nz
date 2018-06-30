2f56543387a55822621edcbbc0442e84f12a3992
Mike Mudge <mike.mudge@gmail.com>
2018-06-30 21:50:41 +1200

    testing push

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -1,3 +1,7 @@
 CREATE DATABASE "mudgeconz";
 CREATE USER "mudgeconz" WITH PASSWORD 'xXBE3RcJi1ULfeo3LKENqdBZqmtzLI';
 GRANT ALL PRIVILEGES ON DATABASE "mudgeconz" to "mudgeconz";
+
+CREATE DATABASE "mudgeconzTest"
+CREATE USER "mudgeconzTest" WITH PASSWORD 'test_password';
+GRANT ALL PRIVILEGES ON DATABASE "mudgeconzTest" to "mudgeconzTest";

850bdac880f8d2ea921ffa8097763f84760d7ed3
Mike Mudge <mike.mudge@gmail.com>
2018-08-04 12:34:19 +1200

    Add better commands, fix friends page css.

--- shared/init_command.py
+++ shared/init_command.py
@@ -2,8 +2,6 @@ from flask_script import Manager
 from flask import current_app
 
 from shared.database import db
-from apps.tournament_app.models import Tournament, Team, Match, Round
-from apps.tournament_app.helpers import tournament as tournament_helper
 from auth.models import Client, Scope, User
 from apps.trail.models import Trail
 
@@ -64,35 +62,5 @@ def trails():
         trail_url='pacific_coast.json'
     )
     db.session.add(trail)
-
-    db.session.commit()
-
-@InitCommand.command
-def tournaments(reset=False):
-
-    if reset:
-        print("Removing all Tournament models")
-        Round.query.delete()
-        Match.query.delete()
-        Team.query.delete()
-        Tournament.query.delete()
-    tournament = Tournament.query.filter_by(name="Test Tournament").first()
-
-    if not tournament:
-        print("Create tournament")
-        tournament = Tournament(name="Test Tournament")
-        db.session.add(tournament)
-    else:
-        print("Already exists")
-        # Clear rounds/matches and teams and recreate.
-        tournament.rounds.delete()
-        tournament.teams.delete()
-
-    # Now fill in the details of the tournament.
-    numTeams = 10
-    teams = [
-        Team(name='Team %d' % (i + 1)) for i in range(0, numTeams)
-    ]
-    tournament_helper.makeFromTeams(tournament, teams)
 
     db.session.commit()

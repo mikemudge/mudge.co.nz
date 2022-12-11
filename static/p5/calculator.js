7b1fdf652e65838b469d517ca068063b6f268824
Michael Mudge <mmudge@sailthru.com>
2022-12-11 21:06:29 +1300

    [RECIPE_CALCULATOR] Adding more recipes

--- static/p5/calculator.js
+++ static/p5/calculator.js
@@ -19,23 +19,55 @@ function satisfactoryInit() {
   recipes['copper ingot'] = new Recipe({'copper ore': 1}, 2);
   recipes['concrete'] = new Recipe({'limestone': 3}, 4);
 
-  recipes['iron plate'] = new Recipe({'iron ingot': 3 / 2}, 6 / 2);
-  recipes['iron rod'] = new Recipe({'iron ingot': 1}, 4);
-  recipes['screw'] = new Recipe({'iron ingot': 1 / 4}, 24 / 20);
-
-  recipes['reinforced iron plate'] = new Recipe({'iron plate': 6, 'screw': 12}, 12);
-
-  recipes['modular frame'] = new Recipe({'iron rod': 12 / 2, 'reinforced iron plate': 3 / 2}, 60 / 2);
-
   // recipes['steel ingot'] = new Recipe({'coal': 3 / 3, 'iron ore': 3 / 3}, 4 / 3);
   // TODO alternate recipes?
   recipes['steel ingot'] = new Recipe({'coal': 2 / 3, 'iron ingot': 2 / 3}, 3 / 3);
 
+  // Constructor parts
+  recipes['iron plate'] = new Recipe({'iron ingot': 3 / 2}, 6 / 2);
+  recipes['iron rod'] = new Recipe({'iron ingot': 1}, 4);
+  recipes['screw'] = new Recipe({'iron ingot': 1 / 4}, 24 / 20);
+
+  recipes['wire'] = new Recipe({'copper ingot': 1 / 2}, 4 / 2);
+  recipes['cable'] = new Recipe({'wire': 2}, 2);
+
+  recipes['silica'] = new Recipe({'raw quartz': 3 / 5}, 8 / 5);
+  recipes['quartz crystal'] = new Recipe({'raw quartz': 5 / 3}, 8 / 3);
+
   recipes['steel pipe'] = new Recipe({'steel ingot': 3 / 2}, 6 / 2);
   recipes['steel beam'] = new Recipe({'steel ingot': 4}, 4);
 
+  // Assembler parts.
+  recipes['reinforced iron plate'] = new Recipe({'iron plate': 6, 'screw': 12}, 12);
+
+  recipes['modular frame'] = new Recipe({'iron rod': 12 / 2, 'reinforced iron plate': 3 / 2}, 60 / 2);
+
   recipes['encased industrial beam'] = new Recipe({'steel beam': 4, 'concrete': 5}, 10);
 
+  recipes['rotor'] = new Recipe({'iron rod': 5, 'screw': 25}, 15);
+  recipes['stator'] = new Recipe({'steel pipe': 3, 'wire': 8}, 12);
+
+  recipes['motor'] = new Recipe({'rotor': 2, 'stator': 2}, 12);
+
+  // Aluminum chain.
+  recipes['aluminum solution'] = new Recipe({
+    'bauxite': 10 / 12,
+    'water': 10 / 12
+  }, 3 / 12);
+
+  // TODO handle extra outputs, E.g water here.
+  recipes['aluminum scrap'] = new Recipe({
+    'alumina solution': 4 / 6,
+    'coal': 2 / 6
+  }, 1 / 6);
+
+  recipes['aluminum ingot'] = new Recipe({
+    'aluminum scrap': 6 / 4,
+    'silica': 5 / 4
+  }, 4 / 4);
+
+  // recipes['aluminum casing'] - new Recipe({}, ?)
+  // Fused modular frame chain.
   // TODO could pass in 3 as a multiplier value to reduce floating point usage?
   recipes['heavy modular frame'] = new Recipe({
     'modular frame': 8 / 3,
@@ -48,6 +80,27 @@ function satisfactoryInit() {
     'heavy modular frame': 1,
     'aluminum casing': 50,
     'nitrogen gas': 25
+  }, 40);
+
+  // Turbo motor chain.
+  recipes['cooling system'] = new Recipe({
+    'heat sink': 2,
+    'rubber': 2,
+    'water': 5,
+    'nitrogen gas': 25
+  }, 10);
+
+  recipes['radio control unit'] = new Recipe({
+    'aluminum casing': 32 / 2,
+    'crystal oscillator': 1 / 2,
+    'computer': 1 / 2
+  }, 2.5 * 60);
+
+  recipes['turbo motor'] = new Recipe({
+    'cooling system': 4,
+    'radio control unit': 2,
+    'motor': 4,
+    'rubber': 24
   }, 40);
 }
 

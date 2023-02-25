4131237d384af28f053a7f956168352202c32b4d
Michael Mudge <mmudge@sailthru.com>
2023-02-26 10:50:55 +1300

    Display possible moves for each piece

--- static/p5/chess.js
+++ static/p5/chess.js
@@ -35,6 +35,20 @@ class Board {
     this.x = 50;
     this.y = 50;
     this.size = 20;
+
+    this.cardinals = [
+      {x: 0, y: 1},
+      {x: 1, y: 0},
+      {x: 0, y: -1},
+      {x: -1, y: 0},
+    ];
+    this.diagonals = [
+      {x: 1, y: 1},
+      {x: 1, y: -1},
+      {x: -1, y: -1},
+      {x: -1, y: 1},
+    ];
+    this.dirs = this.cardinals.concat(this.diagonals);
   }
 
   show() {
@@ -48,12 +62,7 @@ class Board {
     }
 
     if (this.selectedUnit) {
-      stroke(color(20, 250, 20));
-      strokeWeight(2);
-      noFill();
-      let x = this.x + this.selectedUnit.x * this.size * 2 + this.size;
-      let y = this.y + this.selectedUnit.y * this.size * 2 + this.size;
-      circle(x, y, this.size * 1.5);
+      this.showSelectedUnit(this.selectedUnit);
     }
   }
 
@@ -83,32 +92,142 @@ class Board {
   initStart() {
     this.turn = 0;
     for (let x = 0; x < 8; x++) {
-      this.grid[1][x].set(new Unit(this, x, 1, color(10, 10, 10), "P"))
-      this.grid[6][x].set(new Unit(this, x, 6, color(240, 240, 240), "P"))
+      this.grid[1][x].set(new Unit(this, x, 1, "B", "P"))
+      this.grid[6][x].set(new Unit(this, x, 6, "W", "P"))
     }
 
-    this.addUnit(new Unit(this, 0, 0, color(10, 10, 10), "R"))
-    this.addUnit(new Unit(this, 1, 0, color(10, 10, 10), "H"))
-    this.addUnit(new Unit(this, 2, 0, color(10, 10, 10), "B"))
-    this.addUnit(new Unit(this, 3, 0, color(10, 10, 10), "K"))
-    this.addUnit(new Unit(this, 4, 0, color(10, 10, 10), "Q"))
-    this.addUnit(new Unit(this, 5, 0, color(10, 10, 10), "B"))
-    this.addUnit(new Unit(this, 6, 0, color(10, 10, 10), "H"))
-    this.addUnit(new Unit(this, 7, 0, color(10, 10, 10), "R"))
+    this.addUnit(new Unit(this, 0, 0, "B", "R"))
+    this.addUnit(new Unit(this, 1, 0, "B", "H"))
+    this.addUnit(new Unit(this, 2, 0, "B", "B"))
+    this.addUnit(new Unit(this, 3, 0, "B", "K"))
+    this.addUnit(new Unit(this, 4, 0, "B", "Q"))
+    this.addUnit(new Unit(this, 5, 0, "B", "B"))
+    this.addUnit(new Unit(this, 6, 0, "B", "H"))
+    this.addUnit(new Unit(this, 7, 0, "B", "R"))
 
-    this.addUnit(new Unit(this, 0, 7, color(240, 240, 240), "R"))
-    this.addUnit(new Unit(this, 1, 7, color(240, 240, 240), "H"))
-    this.addUnit(new Unit(this, 2, 7, color(240, 240, 240), "B"))
-    this.addUnit(new Unit(this, 3, 7, color(240, 240, 240), "K"))
-    this.addUnit(new Unit(this, 4, 7, color(240, 240, 240), "Q"))
-    this.addUnit(new Unit(this, 5, 7, color(240, 240, 240), "B"))
-    this.addUnit(new Unit(this, 6, 7, color(240, 240, 240), "H"))
-    this.addUnit(new Unit(this, 7, 7, color(240, 240, 240), "R"))
+    this.addUnit(new Unit(this, 0, 7, "W", "R"))
+    this.addUnit(new Unit(this, 1, 7, "W", "H"))
+    this.addUnit(new Unit(this, 2, 7, "W", "B"))
+    this.addUnit(new Unit(this, 3, 7, "W", "K"))
+    this.addUnit(new Unit(this, 4, 7, "W", "Q"))
+    this.addUnit(new Unit(this, 5, 7, "W", "B"))
+    this.addUnit(new Unit(this, 6, 7, "W", "H"))
+    this.addUnit(new Unit(this, 7, 7, "W", "R"))
 
   }
 
   addUnit(unit) {
     this.grid[unit.y][unit.x].set(unit);
+  }
+
+  showSelectedUnit(selectedUnit) {
+    stroke(color(20, 250, 20));
+    strokeWeight(2);
+    noFill();
+    let x = this.x + this.selectedUnit.x * this.size * 2 + this.size;
+    let y = this.y + this.selectedUnit.y * this.size * 2 + this.size;
+    circle(x, y, this.size * 1.5);
+
+    // Now show the possible moves.
+    let moves = this.possibleMoves(this.selectedUnit);
+    fill(color(20, 250, 20));
+    noStroke();
+    for (let move of moves) {
+      let x = this.x + move.x * this.size * 2 + this.size;
+      let y = this.y + move.y * this.size * 2 + this.size;
+      circle(x, y, this.size * .5);
+    }
+  }
+
+  possibleMoves(selectedUnit) {
+    switch (selectedUnit.unit) {
+      case "P":
+        if (selectedUnit.color === "W") {
+          return [{x: selectedUnit.x, y: selectedUnit.y - 1}];
+        } else {
+          return [{x: selectedUnit.x, y: selectedUnit.y + 1}];
+        }
+      case "K":
+        return this.kingMoves(selectedUnit);
+      case "H":
+        return this.knightMoves(selectedUnit);
+      case "B":
+        return this.bishopMoves(selectedUnit);
+      case "R":
+        return this.rookMoves(selectedUnit);
+      case "Q":
+        return this.queenMoves(selectedUnit);
+      default:
+        throw new Error("Not a known unit", selectedUnit);
+    }
+  }
+
+  knightMoves(unit) {
+    let moves = [];
+    for (let dir of this.diagonals) {
+      // Consider moving 2 on x and 1 on y
+      let x = unit.x + dir.x * 2;
+      let y = unit.y + dir.y;
+      if (this.isValid(x, y, unit)) {
+        moves.push({x: x, y: y});
+      }
+      // Also consider moving 1 on x and 2 on y
+      x = unit.x + dir.x;
+      y = unit.y + dir.y * 2;
+      if (this.isValid(x, y, unit)) {
+        moves.push({x: x, y: y});
+      }
+    }
+    return moves;
+  }
+
+  kingMoves(unit) {
+    return this.moves(unit, this.dirs, 1);
+  }
+
+  bishopMoves(unit) {
+    return this.moves(unit, this.diagonals, 8);
+  }
+
+  rookMoves(unit) {
+    return this.moves(unit, this.cardinals, 8);
+  }
+
+  queenMoves(unit) {
+    return this.moves(unit, this.dirs, 8);
+  }
+
+  moves(unit, dirs, maxDis) {
+    let moves = [];
+    for (let dir of dirs) {
+      for (let dis = 1; dis <= maxDis; dis++) {
+        let x = unit.x + dir.x * dis;
+        let y = unit.y + dir.y * dis;
+        if (this.isValid(x, y, unit)) {
+          moves.push({x: x, y: y});
+        } else {
+          break;
+        }
+        let capture = this.grid[y][x].get();
+        if (capture) {
+          // If there is a piece here we can't go past it.
+          break;
+        }
+      }
+    }
+    return moves;
+  }
+
+  isValid(x, y, unit) {
+    if (x < 0 || x >= 8 || y < 0 || y >= 8) {
+      return false;
+    }
+    let capture = this.grid[y][x].get();
+    if (capture && capture.color === unit.color) {
+      // Can't capture your own pieces.
+      return false;
+    }
+    return true;
   }
 }
 
@@ -126,7 +245,11 @@ class Unit {
     let y = this.board.y + this.y * size * 2 + size;
     stroke(color(128,128,128));
     strokeWeight(2);
-    fill(this.color);
+    if (this.color === "W") {
+      fill(color(240, 240, 240));
+    } else {
+      fill(color(10, 10, 10));
+    }
     circle(x, y, size);
     fill(color(128,128,128));
     noStroke();

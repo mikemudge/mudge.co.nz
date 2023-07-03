c6579d021501d6a077d2a9edfaca504bcfd50252
Michael Mudge <mmudge@sailthru.com>
2023-07-03 17:18:05 +1200

    Add energy based spring physics.

--- static/p5/spring.js
+++ static/p5/spring.js
@@ -14,20 +14,36 @@ class Node {
       return;
     }
 
+    stroke(color('cyan'));
+    strokeWeight(2);
+
     // Start with some gravity.
-    let force = createVector(0, .1);
+    let force = createVector(0, 10);
+    line(this.pos.x, this.pos.y, this.pos.x + force.x * 100, this.pos.y + force.y * 100);
     // Add some air resistance.
-    let resistance = this.vel.copy().setMag(- this.vel.magSq() / 1000);
+    let resistance = this.vel.copy().setMag(- this.vel.magSq() / 20);
+    line(this.pos.x, this.pos.y, this.pos.x + resistance.x * 100, this.pos.y + resistance.y * 100);
     force.add(resistance);
 
     // Then add the forces towards the natural length of the connections.
     for (let conn of this.connections) {
       let f = conn.force(this);
+      line(this.pos.x, this.pos.y, this.pos.x + f.x * 100, this.pos.y + f.y * 100);
       force.add(f);
     }
+    // Scale the force by the time per frame to calculate the velocity as a distance per frame.
+    // delta x = v * delta t
+    force.mult(1/30);
+    force = conn.energyBasedForce(node, force);
     this.vel.add(force);
 
-    this.vel.mult(0.99);
+    // Show the total force.
+    stroke(color('red'));
+    line(this.pos.x, this.pos.y, this.pos.x + force.x * 3000, this.pos.y + force.y * 3000);
+
+    // Max speed to avoid the really crazy movements.
+    this.vel.limit(5);
+
     this.pos.add(this.vel);
   }
 
@@ -58,16 +74,69 @@ class Connection {
   constructor(from, to) {
     this.from = from;
     this.to = to;
-    this.rigidity = 1;
+    this.rigidity = 5;
     this.length = this.from.pos.dist(this.to.pos);
   }
 
-  force(node) {
-    let diff = (this.from.pos.dist(this.to.pos) - this.length);
-    if (node === this.from) {
-      return p5.Vector.sub(this.to.pos, this.from.pos).setMag(diff / 10);
+  energyBasedForce(node, f) {
+    let diff = this.from.pos.dist(this.to.pos) - this.length;
+    let v = node.vel.mag();
+    var m = 1;
+    // energy;
+    // Gravity = m * g * h.
+    // Kinetic = 1/2 m * v * v
+    // Spring = 1/2 k * x * x
+
+    // determine after with the force applied.
+    let nx = (node.x + node.vel.x + f.x);
+    let ny = (node.y + node.vel.y + f.y);
+    let dh = ny - node.y;
+    var dx = createVector(nx, ny).sub(this.from.pos);
+    if (this.from === node) {
+      dx = createVector(nx, ny).sub(this.to.pos).mult(-1);
     }
-    return p5.Vector.sub(this.from.pos, this.to.pos).setMag(diff / 10);
+    let ndiff = dx - this.length;
+
+    var energyDelta = 0;
+    energyDelta += m * 10 * dh;
+    energyDelta += 1 / 2 * this.rigidity * (ndiff * ndiff - diff * diff);
+
+    // energyDelta *= 0.9;
+    // Calculate how much speed the node can have with energy conserved?
+    let nv = Math.sqrt(energyDelta * 2 / m + v * v);
+    f.setMag(nv);
+    return f;
+  }
+
+  force(node) {
+    let diff = this.from.pos.dist(this.to.pos) - this.length;
+
+    if (diff < 0) {
+      // the connection is shorter than expected.
+      // For ropes this would mean 0 tension.
+      // return createVector(0,0);
+    }
+    let directionForce = p5.Vector.sub(this.to.pos, this.from.pos);
+    // Determine the magnitude of force based on spring forces f = -kx
+    directionForce.setMag(-diff * this.rigidity);
+
+    // If the forces movement will flip direction in the space of a single frame then we should apply less.
+    // E.g -kx will be in the opposite direction by the end of this frame.
+
+    if (node === this.from) {
+      // If we want the force from the other node's perspective it would be the equal opposite.
+      directionForce.mult(-1);
+    }
+
+    // Make this a desired speed rather than an applied force.
+    // delta = final - initial
+    // node.vel is initial
+    let desiredSpeed = p5.Vector.sub(directionForce, node.vel);
+    // The force must be applied in the direction of the connection.
+    // But we can scale it based on how much we are already moving in the right direction.
+    directionForce.setMag(desiredSpeed.dot(directionForce));
+
+    return directionForce;
   }
   show() {
     // Green if the length is accurate.
@@ -104,14 +173,13 @@ function draw() {
   background(color(192, 192, 192));
 
   for (let node of nodes) {
-    node.update();
-  }
-
-  for (let node of nodes) {
     node.showConnections();
   }
   for (let node of nodes) {
     node.show();
+  }
+  for (let node of nodes) {
+    node.update();
   }
 }
 

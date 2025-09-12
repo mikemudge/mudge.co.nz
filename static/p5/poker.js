308cd9f226bbfc163b8aa074c9d4ecc083a544b4
Michael Mudge <mmudge@sailthru.com>
2025-09-12 16:38:33 -0700

    Add more hand score detection to poker.

--- static/p5/poker.js
+++ static/p5/poker.js
@@ -24,7 +24,8 @@ class Player {
 
 class Card {
   static SUITS = ["Hearts", "Diamonds", "Clubs", "Spades"];
-  static VALUES = [0, '1','2','3','4','5','6','7','8','9','10','j','q','k'];
+  static NAME_VALUES = [0, 'Ace','2','3','4','5','6','7','8','9','10','Jack','Queen','King'];
+  static IMAGE_VALUES = [0, '1','2','3','4','5','6','7','8','9','10','J','Q','K'];
   constructor(image, suit, value) {
     this.image = image;
     this.suit = suit;
@@ -101,7 +102,10 @@ class PokerGame {
 
   calculateHand(player) {
     var cards = player.hand.concat(this.flop);
-    var values = Array.apply(null, new Array(14)).map(Number.prototype.valueOf,0);
+    var values = [];
+    for (var i = 0; i <= 14; i++) {
+      values.push(0);
+    }
     var suits = {
       'Hearts': 0,
       'Diamonds': 0,
@@ -113,17 +117,20 @@ class PokerGame {
     var flushSuit = null;
     cards.forEach(function(card) {
       values[card.value]++;
-      var count = values[card.value];
+      let count = values[card.value];
       if (count > maxCount) {
         maxCount = count;
         bestCountCard = card;
-      } else if (count == maxCount) {
-        if (bestCountCard.value < card.value) {
-          bestCountCard = card;
+      } else if (count === maxCount) {
+        // Ace is high, so nothing is bigger than 1.
+        if (bestCountCard.value > 1) {
+          if (bestCountCard.value < card.value || card.value === 1) {
+            bestCountCard = card;
+          }
         }
       }
       suits[card.suit]++;
-      var count = suits[card.suit];
+      count = suits[card.suit];
       if (count >= 5) {
         // high card for this suit?
         flushSuit = card.suit;
@@ -131,20 +138,77 @@ class PokerGame {
     });
 
     // Straight check?
-    // Full house check?
 
-    if (maxCount == 1) {
-      player.setScore('High card (' + bestCountCard.value + ')');
+    if (maxCount === 1) {
+      player.setScore('High card ' + Card.NAME_VALUES[bestCountCard.value]);
       return;
     }
-    player.setScore(maxCount + ' of a kind (' + bestCountCard.value + '\'s)');
-    if (flushSuit) {
-      // If 4's then don't overwrite.
-      player.setScore('Flush');
+
+    // Check 2,3 of a kind
+    if (maxCount === 2) {
+      let pair2 = false;
+      for (let [num, value] of values.entries()) {
+        if (num === bestCountCard.value) {
+          // Skip the same value as we know we have a pair of them.
+          continue;
+        }
+        // If there is a pair of anything else, then its a 2 pair.
+        if (value === 2) {
+          pair2 = num;
+          break;
+        }
+      }
+      if (pair2) {
+        player.setScore('2 Pair of ' + Card.NAME_VALUES[bestCountCard.value] + '\'s and ' + Card.NAME_VALUES[pair2] + '\'s');
+      } else {
+        player.setScore('Pair of ' + Card.NAME_VALUES[bestCountCard.value] + '\'s');
+      }
+
     }
+    if (maxCount === 3) {
+      let fullHouse = false;
+      for (let [num, value] of values.entries()) {
+        // If there is a pair of anything else, then its a full house.
+        if (value === 2) {
+          fullHouse = num;
+          break;
+        }
+      }
+      if (fullHouse) {
+        player.setScore('Full House ' + Card.NAME_VALUES[bestCountCard.value] + '\'s and ' + Card.NAME_VALUES[fullHouse] + '\'s');
+      } else {
+        player.setScore('3 of a kind (' + Card.NAME_VALUES[bestCountCard.value] + '\'s)');
+      }
+    }
+    if (flushSuit) {
+      player.setScore('Flush ' + flushSuit + Card.NAME_VALUES[bestCountCard.value] + ' high');
+    }
+    // Check 4 of a kind.
+    if (maxCount === 4) {
+      player.setScore('4 of a kind (' + Card.NAME_VALUES[bestCountCard.value] + '\'s)');
+    }
+
+    // TODO check straight flush/royal flush?
   };
 
+  click(x, y) {
+    if (x > 100 && x < 200) {
+      if (y > 150 && y < 170) {
+        this.makeGame();
+      }
+    }
+  }
+
   show() {
+
+    //button
+    stroke(0);
+    noFill();
+    rect(100, 150, 100, 20);
+    fill(0);
+    noStroke();
+    text("Deal again", 105, 165);
+
     for (let i = 0; i < this.flop.length; i++) {
       this.flop[i].show(100 * i, 50);
     }
@@ -161,7 +225,8 @@ export function preload() {
   for (var i=0;i<52;i++) {
     let suit = Card.SUITS[Math.floor(i / 13)];
     let value = (i % 13) + 1;
-    let image = loadImage('/static/img/cards_png/' + suit.charAt(0).toLowerCase() + Card.VALUES[value] + '.png');
+    let name = (suit.charAt(0) + Card.IMAGE_VALUES[value]).toLowerCase();
+    let image = loadImage('/static/img/cards_png/' + name + '.png');
     cards.push(new Card(image, suit, value));
   }
   game = new PokerGame(cards);
@@ -178,4 +243,8 @@ export function draw() {
   background(255);
 
   game.show();
+}
+
+export function mouseClicked() {
+  game.click(mouseX, mouseY);
 }

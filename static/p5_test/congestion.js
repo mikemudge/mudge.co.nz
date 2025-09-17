c2bd2b9d4b4b840ab3fa61518247b6ec3d9ee259
Michael Mudge <mmudge@sailthru.com>
2025-09-17 16:56:50 -0700

    Introduce a direction system for traffic.

--- static/p5_test/congestion.js
+++ static/p5_test/congestion.js
@@ -26,6 +26,11 @@ class Car {
     }
 
     this.pos.add(this.vel);
+    let next = this.game.grid.getTileAtPosWithSize(this.pos, this.game.size);
+    if (next.getData().car && next.getData().car !== this) {
+      // Can't move forward as there is already a car there.
+      this.pos.sub(this.vel);
+    }
   }
 
 
@@ -45,21 +50,37 @@ class Car {
     return !this.target.getTarget(this);
   }
 
-  show() {
-    fill(this.color);
+  show(size) {
+    let r = this.r * size;
     rectMode(CENTER);
     rotate(this.vel.heading());
-    rect(0, 0, this.r * 2, this.r);
+    fill(color(255,255,150,50));
+    // d is how far from center the light comes from.
+    let d = r / 3;
+    arc(r / 2, d, r * 10, r * 10, -.1, .15);
+    arc(r / 2, -d, r * 10, r * 10, -.15, .1);
+
+    // draw the car on top of the light.
+    fill(this.color);
+    rect(0, 0, r * 2, r);
   }
 }
 
 class Square {
+  constructor() {
+    this.directions = [];
+  }
   show(size) {
     if (this.road) {
       noStroke();
       fill('grey');
       rect(0, 0, size, size);
     }
+    // if (this.directions) {
+    //   textSize(size / 2);
+    //   fill('white')
+    //   text(this.directions, 5, 15);
+    // }
   }
 }
 
@@ -84,9 +105,18 @@ class MazeRouter {
           // Can't walk over OOB or non road tiles.
           continue;
         }
-        for (let t of e.getCardinalTiles()) {
+        // We know e can get to the target by following "next".
+        // Now we want to consider what can reach e by iterating through its neighbours.
+        for (let [d, t] of e.getCardinalTiles().entries()) {
           if (this.grid.getTile(t.x, t.y).getData()) {
             continue;
+          }
+          let opp = (d + 2) % 4;
+          if (t.getData() && t.getData().directions) {
+            if (!t.getData().directions.includes(opp)) {
+              // The square has directions, and opp is not one of them.
+              continue;
+            }
           }
           this.grid.getTile(t.x, t.y).setData({"next": e});
           next.push(t);
@@ -110,53 +140,67 @@ class MazeRouter {
 
 class CongestionGame {
   constructor(view) {
+    const params = new URLSearchParams(window.location.search);
     this.view = view;
     this.size = view.getMapSize();
     this.time = 0;
+    this.spawnRate = params.get("spawnRate") || 100;
     this.cars = [];
 
-    this.grid = new Grid(39, 24);
+    this.grid = new Grid(45, 28);
     // Move the center of the view to the center of the grid its looking at.
     this.view.setGridCenter(this.grid);
 
+    this.spawners = [];
     // Init road tiles.
     for (let y = 0; y < this.grid.getHeight(); y++) {
       for (let x = 0; x < this.grid.getWidth(); x++) {
-        if (x % 5 === 4 || y % 5 === 4) {
+        if (x % 15 === 7 || y % 15 === 7 || x % 15 === 8 || y % 15 === 8) {
           let square = new Square();
           square.road = true;
           this.grid.setTileData(x, y, square);
+          if (x % 15 === 7) {
+            square.directions.push(0);
+          }
+          if (x % 15 === 8) {
+            square.directions.push(2);
+          }
+          if (y % 15 === 7) {
+            square.directions.push(1);
+          }
+          if (y % 15 === 8) {
+            square.directions.push(3);
+          }
+        }
+        if (y === 0 && x % 15 === 8) {
+          this.spawners.push(this.grid.getTile(x, y));
+        }
+        if (y === this.grid.getHeight() - 1 && x % 15 === 7) {
+          this.spawners.push(this.grid.getTile(x, y));
+        }
+        if (x === 0 && y % 15 === 7) {
+          this.spawners.push(this.grid.getTile(x, y));
+        }
+        if (x === this.grid.getWidth() - 1 && y % 15 === 8) {
+          this.spawners.push(this.grid.getTile(x, y));
         }
       }
     }
 
-    // Now setup some spawners and destinations with routes.
-    this.spawners = [
-      this.grid.getTile(0, 9),
-      this.grid.getTile(38, 9),
-      this.grid.getTile(0, 19),
-      this.grid.getTile(38, 19),
-      this.grid.getTile(4, 0),
-      this.grid.getTile(9, 23),
-      this.grid.getTile(14, 0),
-      this.grid.getTile(19, 23),
-      this.grid.getTile(24, 0),
-      this.grid.getTile(29, 23),
-      this.grid.getTile(34, 0),
-    ];
-
+    // Now setup some destinations with routes.
     this.destinations = [];
-    this.addDestination('#FF0000', this.grid.getTile(38, 4));
-    this.addDestination('#FF8000', this.grid.getTile(0, 4));
-    this.addDestination('#FFFF00', this.grid.getTile(38, 14));
-    this.addDestination('#00FF00', this.grid.getTile(0, 14));
-    this.addDestination('#00FFFF', this.grid.getTile(34, 23));
-    this.addDestination('#0080FF', this.grid.getTile(29, 0));
-    this.addDestination('#0000FF', this.grid.getTile(24, 23));
-    this.addDestination('#8000FF', this.grid.getTile(19, 0));
-    this.addDestination('#FF00FF', this.grid.getTile(14, 23));
-    this.addDestination('#FF0080', this.grid.getTile(9, 0));
-    this.addDestination('#800080', this.grid.getTile(4, 23));
+    this.addDestination('#FF0000', this.grid.getTile(this.grid.getWidth() - 1, 7));
+    this.addDestination('#FF8000', this.grid.getTile(this.grid.getWidth() - 1, 22));
+    this.addDestination('#FFFF00', this.grid.getTile(0, 8));
+    this.addDestination('#00FF00', this.grid.getTile(0, 23));
+
+    this.addDestination('#00FFFF', this.grid.getTile(8, this.grid.getHeight() - 1));
+    this.addDestination('#0080FF', this.grid.getTile(23, this.grid.getHeight() - 1));
+    this.addDestination('#0000FF', this.grid.getTile(38, this.grid.getHeight() - 1));
+    this.addDestination('#8000FF', this.grid.getTile(7, 0));
+    this.addDestination('#FF00FF', this.grid.getTile(22, 0));
+    this.addDestination('#FF0080', this.grid.getTile(37, 0));
+    // this.addDestination('#800080', this.grid.getTile(34, 0));
   }
 
   addDestination(color, target) {
@@ -180,19 +224,25 @@ class CongestionGame {
   update() {
     this.time++;
     // Add new cars when appropriate
-    if (this.time % 300 === 1) {
+    if (this.time % this.spawnRate === 1) {
       for (let spawn of this.spawners) {
         this.createNewCar(spawn);
       }
     }
 
     for (let car of this.cars) {
+      let b4 = this.grid.getTileAtPosWithSize(car.pos, this.size);
       car.update();
+      let after = this.grid.getTileAtPosWithSize(car.pos, this.size);
+      // Update the tile which the car is associated with.
+      b4.getData().car = null;
+      after.getData().car = car;
     }
 
     // Remove cars once they leave the playing area.
     for (let i = this.cars.length - 1; i >= 0; i--) {
       if (this.cars[i].finished()) {
+        this.grid.getTileAtPosWithSize(this.cars[i].pos, this.size).getData().car = null;
         this.cars.splice(i, 1);
       }
     }
@@ -220,6 +270,18 @@ class CongestionGame {
         text("F", size / 4, size * .85);
       });
     }
+
+    this.view.coverEdges();
+
+    // TODO can this become part of a common game class?
+    if (this.paused) {
+      textAlign(CENTER);
+      textSize(20);
+      fill('#FFFFFF');
+      text("Paused!", windowWidth / 2, windowHeight / 2);
+      textSize(10)
+      text("Click to resume", windowWidth / 2, windowHeight / 2 + 20);
+    }
   }
 }
 
@@ -230,6 +292,11 @@ export function setup() {
   view.createCanvas()
 
   game = new CongestionGame(view);
+
+  window.onblur = function() {
+    game.paused = true;
+    noLoop();
+  }
 }
 
 export function draw() {
@@ -237,6 +304,19 @@ export function draw() {
 
   game.update();
   game.show();
+}
+export function mouseClicked() {
+  if (game.paused) {
+    game.paused = false;
+    loop();
+    return;
+  }
+}
+
+export function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+
+  view.setScreen(windowWidth, windowHeight);
 }
 
 export function mouseWheel(event) {
